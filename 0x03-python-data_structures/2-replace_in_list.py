076751bfe192020952df355c6b4499a06667c11d
miringu7 <miringusimeon@gmail.com>
2024-01-08 11:48:20 +0300

    fixing errors

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -5,7 +5,7 @@ def replace_in_list(my_list, idx, element):
         at a specific position (like in C)."""
     if idx < 0:
         return my_list
-    elif idx (len(my_list) - 1):
+    elif idx > (len(my_list) - 1):
         return my_list
     my_list[idx] = element
     return my_list
