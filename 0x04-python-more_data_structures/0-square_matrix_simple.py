ba5ead382a4581f7ef9431361bdf24d259951ebf
miringu7 <miringusimeon@gmail.com>
2024-01-10 20:37:42 +0300

    0. Squared simple

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    """function that computes the square value of all integers of a matrix."""
+    new_matrix = [[i ** 2 for i in row] for row in matrix]
+    return new_matrix
