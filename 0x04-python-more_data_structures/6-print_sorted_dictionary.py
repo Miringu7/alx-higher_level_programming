6744c95a5a93634cebca4144ebfa830c8c7049e7
miringu7 <miringusimeon@gmail.com>
2024-01-10 21:51:51 +0300

    fixing errors

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -4,4 +4,4 @@ def print_sorted_dictionary(a_dictionary):
     """function that prints a dictionary by ordered keys."""
     sort_a = sorted(a_dictionary)
     for i in sort_a:
-        print("{}: {}".format(i, a_dictionary[i], end = "\n"))
+        print("{}: {}".format(i, a_dictionary[i], end="\n"))
