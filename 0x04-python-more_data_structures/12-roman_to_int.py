aaa9b94b5fcf58c7abcfb0ef30dbd255a5a241cc
miringu7 <miringusimeon@gmail.com>
2024-01-10 23:50:08 +0300

    fixing errors

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,30 +3,32 @@
 def roman_to_int(roman_string):
     """function def roman_to_int(roman_string): that
         converts a Roman numeral to an integer."""
-    roman_dict = {
-        'I': 1,
-        'IV': 4,
-        'V': 5,
-        'IX': 9,
-        'X': 10,
-        'XL': 40,
-        'L': 50,
-        'XC': 90,
-        'C': 100,
-        'CD': 400,
-        'D': 500,
-        'CM': 900,
-        'M': 1000
-    }
+    if roman_string:
+        roman_dict = {
+            'I': 1,
+            'IV': 4,
+            'V': 5,
+            'IX': 9,
+            'X': 10,
+            'XL': 40,
+            'L': 50,
+            'XC': 90,
+            'C': 100,
+            'CD': 400,
+            'D': 500,
+            'CM': 900,
+            'M': 1000
+        }
 
-    total = 0
-    i = 0
+        total = 0
+        i = 0
 
-    while i < len(roman_string):
-        if i + 1 < len(roman_string) and roman_string[i:i+2] in roman_dict:
-            total += roman_dict[roman_string[i:i+2]]
-            i += 2
-        else:
-            total += roman_dict[roman_string[i]]
-            i += 1
-    return total
+        while i < len(roman_string):
+            if i + 1 < len(roman_string) and roman_string[i:i+2] in roman_dict:
+                total += roman_dict[roman_string[i:i+2]]
+                i += 2
+            else:
+                total += roman_dict[roman_string[i]]
+                i += 1
+        return total
+    return 0
