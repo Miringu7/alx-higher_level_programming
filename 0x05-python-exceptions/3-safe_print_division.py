8141f21fb1736a0761126129252e951017874cd3
miringu7 <miringusimeon@gmail.com>
2024-01-22 23:51:53 +0300

    fixing errors

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -8,6 +8,6 @@ def safe_print_division(a, b):
     except (ZeroDivisionError, ValueError, TypeError):
         return None
     finally:
-        print("Inside result: ", "{:.1f}".format(div_result) if div_result
+        print("Inside result:", "{:.1f}".format(div_result) if div_result
               is not None else "None")
     return div_result
