0374a70544f319c6aaf2c41a3f8da65288044e21
miringu7 <miringusimeon@gmail.com>
2024-01-31 17:36:45 +0300

    fixing errors

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -15,12 +15,12 @@ def matrix_divided(matrix, div):
         ZeroDivisionError: division by zero
     """
 
-        if (not isinstance(matrix, list) or matrix == [] or
-            not all(isinstance(row, list) for row in matrix) or
-            not all((isinstance(ele, int) or isinstance(ele, float))
-                    for ele in [num for row in matrix for num in row])):
+    if (not isinstance(matrix, list) or matrix == [] or
+        not all(isinstance(row, list) for row in matrix) or
+        not all((isinstance(ele, int) or isinstance(ele, float))
+                for ele in [num for row in matrix for num in row])):
         raise TypeError("matrix must be a matrix (list of lists) of "
-                        "integers/floats")
+                    "integers/floats")
 
     if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
