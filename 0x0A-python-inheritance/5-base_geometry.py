f5a1955b769a981151015508eb9389ddde9500c2
miringu7 <miringusimeon@gmail.com>
2024-02-05 21:46:18 +0300

    5. Geometry module

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+""" defines an empty class """
+
+
+class BaseGeometry():
+    """ Implements an empty class """
+
+    pass
--- 0x0A-python-inheritance/5-main.py
+++ 0x0A-python-inheritance/5-main.py
@@ -1,4 +1,3 @@
-guillaume@ubuntu:~/0x0A$ cat 5-main.py
 #!/usr/bin/python3
 BaseGeometry = __import__('5-base_geometry').BaseGeometry
 
