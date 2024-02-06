66b2cd6229ab8209f46eda5057c2a43bae3832f8
miringu7 <miringusimeon@gmail.com>
2024-02-06 20:24:01 +0300

    3. To JSON string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import json
+"""Defines an json function."""
+
+
+def to_json_string(my_obj):
+    """ function that  returns the JSON representation of an object (string):
+
+    Args:
+        my_obj: object to turn to string
+    Return:
+         JSON representation of an object (string):
+    """
+    return json.dumps(my_obj)
