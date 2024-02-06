77d3638b00d037dfc7deba2c087eb2cbf170fce7
miringu7 <miringusimeon@gmail.com>
2024-02-06 18:02:11 +0300

    fixing errors

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 
+
 def read_file(filename=""):
-    """ function that reads a text file (UTF8) and prints it to stdout:"""
+    """ function that reads a text file (UTF8) and prints it to stdout:
+
+    Args:
+        filename [file]: file to be read
+    """
     with open(filename, encoding="utf-8") as my_utf:
         print(my_utf.read(), end="")
