db36f15df722d40ce5dff1b5b0dfcf2b08f0f566
miringu7 <miringusimeon@gmail.com>
2024-04-12 20:07:30 +0300

    fixing errors

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -12,11 +12,10 @@ if __name__ == "__main__":
     script that takes in an argument and displays all values in states table
     of hbtn_0e_0_usa where name matches the argument.
     """
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=sys.argv[1], passwd=sys.argv[2],
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
                          db=sys.argv[3])
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM `states` WHERE BINARY `name` == {}"
-                   .format(sys.argv[4]))
+    cursor.execute("SELECT * FROM `states` WHERE BINARY
+                   `name` == {}"the.format(sys.argv[4]))
 
     [print(state) for state in cursor.fetchall()]
