f62c9b98d9de46275b32935625cc5e8498d26481
miringu7 <miringusimeon@gmail.com>
2024-04-12 21:34:26 +0300

    script that lists all cities from the database hbtn_0e_4_usa

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -9,16 +9,15 @@ if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     dbname = sys.argv[3]
-    name = sys.argv[4]
 
     db = MySQLdb.connect(host=localhost, port=3306,
                          user=username, passwd=password, db=dbname)
     cursor = db.cursor()
-    query = "SELECT * FROM states WHERE BINARY name = %s ORDER BY states.id ASC"
-    cursor.execute(query, (name,))
-    states = cursor.fetchall()
+    query = "SELECT * FROM cities ORDER BY cities.id ASC"
+    cursor.execute(query)
+    cities = cursor.fetchall()
 
-    [print(state) for state in states]
+    [print(city) for city in cities]
 
     cursor.close()
     db.close()
