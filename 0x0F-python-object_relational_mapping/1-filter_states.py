30d640ad29b0812390df46abeef6316609da9746
miringu7 <miringusimeon@gmail.com>
2024-04-12 18:49:44 +0300

    fixing errors

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -9,34 +9,12 @@ import sys
 import MySQLdb
 
 
-def list_states(username, password, database):
-    """
-    connects to MySQL server and prints the states starting with 'N'
-
-    Args:
-        username: mysql username
-        password: mysql password
-        name: database name
-    """
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=username, passwd=password, db=database)
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
-    states = cursor.fetchall()
-
-    for state in states:
-        if state[0] == "N":
-            print(state)
-
-    cursor.close()
-    db.close()
-
-
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: {} username password database".format(sys.argv[0]))
-        sys.exit(1)
+    # Get MySQL credentials from command-line arguments
+    # Connect to MySQL server
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
 
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
+    # Execute the SQL query to retrieve all states sorted by id
+    c.execute("SELECT * FROM `states` ORDER BY `id`")
+    [print(state) for state in c.fetchall() if state[1][0] == "N"]
