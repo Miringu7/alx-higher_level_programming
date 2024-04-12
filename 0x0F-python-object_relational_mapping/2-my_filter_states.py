3c0cf9d28b85110488811312e17d4e17eeef507c
miringu7 <miringusimeon@gmail.com>
2024-04-12 19:28:07 +0300

    displays all values in the states table of hbtn_0e_0_usa where name matches the argument.

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""
+2. Filter states by user input
+"""
+import sys
+import MySQLdb
+
+
+if __name__ == __main__:
+    """
+    script that takes in an argument and displays all values in the states table
+    of hbtn_0e_0_usa where name matches the argument.
+    """
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM `states` ORDER BY states.id ASC")
+
+    [print(state) for state in cursor.fetchall() if state == sys.argv[4]]
