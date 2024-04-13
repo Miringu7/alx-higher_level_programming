ea5f327857f4308a8dc54567b80d48cab5955a4f
miringu7 <miringusimeon@gmail.com>
2024-04-13 22:48:07 +0300

    fixing errors

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,28 +1,22 @@
 #!/usr/bin/python3
-"""
-Script that lists all State objects from the database hbtn_0e_6_usa
-"""
-
+"""Module that retrieves and prints all\
+        states from a MySQL database using SQLAlchemy."""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
+from model_state import State
 
 if __name__ == "__main__":
-    # Connect to the NySQL server
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+    # Create the SQLAlchemy engine using the provided MySQL credentials
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-
-    # create a session maker
+    # Create a session factory
     Session = sessionmaker(bind=engine)
 
-    # create a session
+    # Create a session object
     session = Session()
 
-    # Query all state objects and sort them by id in ascending order
-    states = session.query(State).order_by(State.id).all()
-
-    # print the results
-    for state in states:
+    # Retrieve all states from the database and print their IDs and names
+    for state in session.query(State).order_by(State.id):
         print("{}: {}".format(state.id, state.name))
