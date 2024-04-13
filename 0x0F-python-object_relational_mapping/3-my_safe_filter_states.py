ac56813c01ab4a1751039092973cf165b8548638
miringu7 <miringusimeon@gmail.com>
2024-04-13 18:14:07 +0300

    fixing errors

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     dbname = sys.argv[3]
     name = sys.argv[4]
 
-    db = MySQLdb.connect(host=localhost, port=3306,
+    db = MySQLdb.connect(host='localhost', port=3306,
                          user=username, passwd=password, db=dbname)
     cursor = db.cursor()
     query = "SELECT * FROM states WHERE BINARY name = %s ORDER BY states.id"
