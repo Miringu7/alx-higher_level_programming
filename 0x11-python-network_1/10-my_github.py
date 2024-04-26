0159ec64696622ca84b0effb8c5376fe4fac4989
miringu7 <miringusimeon@gmail.com>
2024-04-26 22:44:00 +0300

    fixing errors

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-""" script that takes your Github credentials and uses the github api to display id"""
+"""
+script that takes your Github credentials and uses the
+github api to display id
+"""
 import sys
 import requests
 from requests.auth import HTTPBasicAuth
--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -8,4 +8,5 @@ import requests
 if __name__ == "__main__":
     resp = requests.get('https://alx-intranet.hbtn.io/status')
     txt = resp.text
-    print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(txt), txt))
+    print('Body response:\n\t- type: {}\n\t- content: {}'
+            .format(type(txt), txt))
