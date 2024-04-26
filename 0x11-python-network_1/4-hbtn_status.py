490f0b94c3891bfae8d6978db90fdabb1e1df1bb
miringu7 <miringusimeon@gmail.com>
2024-04-26 22:05:16 +0300

    5. Response header value #1

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Python script that fetches an URL with requests package
+"""
+import requests
+
+
+if __name__ == "__main__":
+    resp = requests.get('https://alx-intranet.hbtn.io/status')
+    txt = resp.text
+    print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(txt), txt))
